9c027a8c266b62704ec662860a97068247fd58cd
Igor <nekto.korobko@gmail.com>
2023-03-16 22:23:10 +0300

    rename function post_telegram to send_post create function send_animation_image

--- tg.py
+++ tg.py
@@ -1,6 +1,11 @@
-async def post_telegram(telegram_chat_id, bot, text, image):
+async def send_post(telegram_chat_id, bot, text, image):
     if image:
         message = await bot.send_photo(chat_id=telegram_chat_id, photo=image, caption=text)
     else:
         message = await bot.send_message(chat_id=telegram_chat_id, text=text)
+    return message['message_id']
+
+
+async def send_animation_image(telegram_chat_id, bot, image):
+    message = await bot.sendAnimation(chat_id=telegram_chat_id, animation=image)
     return message['message_id']
