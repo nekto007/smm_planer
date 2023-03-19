ed8ae5727e2cc2b9b7bd9c30816500c5ec447769
Igor <nekto.korobko@gmail.com>
2023-03-19 23:14:31 +0300

    create function deleting from telegram

--- publish_on_tg.py
+++ publish_on_tg.py
@@ -6,6 +6,10 @@ async def send_post(telegram_chat_id, bot, text, image):
     return message['message_id']
 
 
-async def send_animation_image(telegram_chat_id, bot, image):
-    message = await bot.sendAnimation(chat_id=telegram_chat_id, animation=image)
+async def send_animation_image(telegram_chat_id, bot, image, text):
+    message = await bot.sendAnimation(chat_id=telegram_chat_id, animation=image, caption=text)
     return message['message_id']
+
+
+async def delete_tg_post(telegram_chat_id, bot, post_id):
+    await bot.delete_message(chat_id=telegram_chat_id, message_id=post_id)
